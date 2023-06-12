f506ff5980fda2af4bcafd58dc51fecc69de7eb5
Mazen Elhammamy <mazene@topdesk.com>
2023-06-12 13:50:05 +0200

    add scan location and scan assets

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,61 @@
 <template>
   <div id="app">
-    <div class="section" style="width: 500px; margin: auto;">
+    <div v-if="!closeLocationScanner">
+      <h1>scan Location</h1>
+      <div class="section" style="width: 500px; margin: auto">
         <QRScanner
-            v-bind:qrbox="250"
-            v-bind:fps="10"
-            style="width: 500px;"
-            @result="onScan" />
-       
+          :asset="asset"
+          v-bind:qrbox="250"
+          v-bind:fps="10"
+          style="width: 500px"
+          @result="onScan"
+        />
+      </div>
+    </div>
+
+    <div v-else>
+      <h3>Scaned Location: {{ location }}</h3>
+      <button v-if="!asset" @click="openSanner">Scan assets</button>
     </div>
     <div>
-  <h1>Results</h1>
-  <div v-for="(item, index) in items" :key="index">{{item}}</div>
-  
-  
+      <div v-if="asset" class="section" style="width: 500px; margin: auto">
+        <QRScanner
+          :asset="asset"
+          v-bind:qrbox="250"
+          v-bind:fps="10"
+          style="width: 500px"
+          @result="onScan"
+        />
+        <div>
+          <h3>Scaned Assets</h3>
+          <div v-for="(asset, index) in assets" :key="index">{{ asset }}</div>
+        </div>
+      </div>
     </div>
   </div>
-  </template>
-  
-  
-  
-  <script setup lang="ts">
-  import { Html5QrcodeResult } from 'html5-qrcode';
-  import QRScanner from './components/QRCodeScanner.vue';
-  import { ref } from 'vue';
-  
-  const items = ref<string[]>([])
-    function onScan(decodedText: string, decodedResult: Html5QrcodeResult) {
-      items.value.push(decodedText)
-      console.log("decodedText", decodedText);
-      console.log("decodedResult", decodedResult);
-    }
-  
-  
-  </script>
-  
+</template>
+
+<script setup lang="ts">
+import { Html5QrcodeResult } from "html5-qrcode";
+import QRScanner from "./components/QRCodeScanner.vue";
+import { ref } from "vue";
+
+const asset = ref(false);
+const location = ref<string>("");
+const assets = ref<string[]>([]);
+const closeLocationScanner = ref(false);
+
+function onScan(decodedText: string, decodedResult: Html5QrcodeResult) {
+  if (!asset.value) {
+    location.value = decodedText;
+    closeLocationScanner.value = true;
+  } else {
+    assets.value.push(decodedText);
+  }
+  console.log(decodedResult);
+}
+
+function openSanner() {
+  asset.value = true;
+}
+</script>
--- src/components/QRCodeScanner.vue
+++ src/components/QRCodeScanner.vue
@@ -1,36 +1,45 @@
 <template>
-    <div id="qr-code-full-region"></div>
-  </template>
-  
-  <script setup lang="ts">
-  import { onMounted } from 'vue';
-  import { Html5QrcodeResult, Html5QrcodeScanner } from 'html5-qrcode';
-  
-  const qrbox = 250; // Set the size of the QR code scanning region
-  const fps = 1; // Set the desired frames per second
-  const emit = defineEmits(['result'])
-     
-  const onScanSuccess = (decodedText: string, decodedResult: Html5QrcodeResult) => {
-    // Handle the scanned QR code result
-    emit('result',decodedText)
-    console.log('Decoded Text:', decodedText,decodedResult);
-    console.log('Decoded Result:', decodedResult);
+  <div id="qr-code-full-region"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, toRefs } from "vue";
+import { Html5QrcodeResult, Html5QrcodeScanner } from "html5-qrcode";
+
+const props = defineProps({
+  asset: Boolean,
+});
+const { asset } = toRefs(props);
+const qrbox = 250; // Set the size of the QR code scanning region
+const fps = 1; // Set the desired frames per second
+const emit = defineEmits(["result"]);
+let html5QrcodeScanner: Html5QrcodeScanner | null;
+
+const onScanSuccess = (
+  decodedText: string,
+  decodedResult: Html5QrcodeResult
+) => {
+  // Handle the scanned QR code result
+  emit("result", decodedText, decodedResult);
+  console.log("asset", asset.value);
+
+  if (!props.asset) {
+    html5QrcodeScanner?.clear();
+  }
+};
+
+onMounted(() => {
+  const config = {
+    fps: fps,
+    qrbox: qrbox,
   };
-  
-  onMounted(() => {
-    const config = {
-      fps: fps,
-      qrbox: qrbox,
-    };
-  
-    const html5QrcodeScanner = new Html5QrcodeScanner(
-      "qr-code-full-region", // ID of the element to render the scanner
-      config,
-      undefined
-    );
-  
-    html5QrcodeScanner.render(onScanSuccess, ()=>{});
-  });
-  </script>
-  
-  
+
+  html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-code-full-region", // ID of the element to render the scanner
+    config,
+    undefined
+  );
+
+  html5QrcodeScanner.render(onScanSuccess, () => {});
+});
+</script>
